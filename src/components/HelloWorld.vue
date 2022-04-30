16928675e99c9c54554a906cad1e5bd65cb83a0f
Harikrishnan <85402206+hknp796@users.noreply.github.com>
2022-04-30 20:01:01 +0530

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,71 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <v-app>
+      <v-form>
+        <v-container>
+          <v-row>
+            <v-col>
+              <v-text-field
+                v-model="arraysize"
+                label="Enter total socks number"
+                type="number"
+              >
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-text-field
+                v-model="inputarraystring"
+                label="Enter colors with space between"
+                type="text"
+              >
+              </v-text-field>
+              {{ this.inputarraystring }}
+            </v-col>
+          </v-row>
+          <template>
+            <v-btn color="primary" @click="submit">submit</v-btn>
+          </template>
+          <v-row>
+            <br />
+            <p v-if="this.input" class="mt-10">
+              array size is {{ this.totalpairs }}
+            </p>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-app>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      arraysize: 0,
+      inputarraystring: '',
+      inputarray: [],
+      input: false,
+      totalpairs: 0,
+    };
+  },
+  methods: {
+    submit() {
+      this.inputarray = this.inputarraystring.split(' ')
+      // let unique = [...new Set(this.inputarray)];
+      console.log(this.inputarray);
+        let unique = this.inputarray.filter()
+      const sortedobject = {};
+      unique.forEach((x) => {
+        sortedobject[x] = (sortedobject[x] || 0) + 1;
+      });
+      this.totalpairs = 0;
+      for (let i = 0; i < unique.length; i++) {
+        this.totalpairs = this.totalpairs + sortedobject[unique[i]] / 2;
+      }
+      this.input = true;
+    },
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
